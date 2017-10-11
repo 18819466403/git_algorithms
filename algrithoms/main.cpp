ceded9e3c2d80431261aa67eed810d31a70e122e
YOUNA <18819466303@163.com>
2017-10-11 17:51:43 +0800

    first commit

---
+++ algrithoms/main.cpp
@@ -0,0 +1,53 @@
+#pragma ocne
+#include<iostream>
+#include"algritom.h"
+
+template<typename T>
+void show_vec(const T* vec,const size_t rows,const size_t cols) {
+	int k = 0;
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++) {
+			//mat[i][j] = k++;
+			std::cout << vec[k++] << " ";
+		}
+		std::cout << std::endl;
+	}
+}
+
+template<typename T>
+void show_mat(T** &mat, const size_t rows, const size_t cols) {
+	int k = 0;
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++) {
+			std::cout << mat[i][j] << " ";
+		}
+		std::cout << std::endl;
+	}
+}
+
+int main() {
+	int vec[36] = { 0,6,1,5,0,0,
+			  	    6,0,5,0,3,0,
+				    1,5,0,5,6,4,
+				    5,0,5,0,0,2,
+				    0,3,6,0,0,6,
+				    0,0,4,2,6,0 };
+	//show_vec<int>(vec,6,6);
+	int **mat = NULL;
+	std::vector<int> result;
+	set_mat<int>(mat, 6, 6);
+	convert_vec_to_mat<int>(vec, mat, 6, 6);
+   // show_mat<int>(mat, 6, 6);
+
+	//std::cout<<find_min(vec, 36);
+	prim(mat, result,6,6);
+
+	std::cout << "The result:";
+	for (int i = 0; i < result.size(); i++) {
+		std::cout << result[i] << " ";
+	}
+
+
+}
